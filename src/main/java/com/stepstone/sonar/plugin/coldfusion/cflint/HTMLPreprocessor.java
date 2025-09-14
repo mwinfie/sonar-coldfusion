17461bf2681003f3d238b6cb861e955628e9fb39
Matt Winfield <matthew.r.winfield@faa.gov>
2025-09-14 08:52:52 -0500

    fix: Resolve SLF4J logger parameter formatting issue
    - Fixed HTMLPreprocessor line 125 logger.debug() call formatting
    - Moved all parameters to single line to avoid SLF4J Marker confusion
    - Resolves final compilation error: incompatible types java.lang.String to org.slf4j.Marker

--- src/main/java/com/stepstone/sonar/plugin/coldfusion/cflint/HTMLPreprocessor.java
+++ src/main/java/com/stepstone/sonar/plugin/coldfusion/cflint/HTMLPreprocessor.java
@@ -122,8 +122,7 @@ public class HTMLPreprocessor {
             }
             
             if (!processedContent.equals(originalContent)) {
-                logger.debug("File {} was preprocessed - {} chars -> {} chars", 
-                           filePath, originalContent.length(), processedContent.length());
+                logger.debug("File {} was preprocessed - {} chars -> {} chars", filePath, originalContent.length(), processedContent.length());
             }
             
             return processedContent;
